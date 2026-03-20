ce8cad2e56c6b8ce81fff952628ae7d48ca78a60
j6655 <jerryhungps5@gmail.com>
2026-03-20 09:37:53 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PhiloApp from './App.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><PhiloApp /></React.StrictMode>
+)
